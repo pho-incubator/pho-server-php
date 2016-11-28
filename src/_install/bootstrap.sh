1092814861e1bf394f3a8da94b81bad5f69f093e
Groups <esokullu@gmail.com>
2016-11-28 21:41:40 +0200

    latest changes to vagrantfile and bootstrap.sh

--- src/_install/bootstrap.sh
+++ src/_install/bootstrap.sh
@@ -18,13 +18,6 @@ sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again p
 sudo apt-get -y install mysql-server
 sudo apt-get install php5-mysql
 
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get -y install phpmyadmin
-
 # setup hosts file
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
@@ -53,12 +46,6 @@ sudo apt-get -y install openssl
 
 # install PHP GD, the graphic lib (we create captchas and avatars)
 sudo apt-get -y install php5-gd
-
-# install git
-sudo apt-get -y install git
-
-# git clone HUGE
-sudo git clone https://github.com/phonetworks/pho-server "/var/www/html/${PROJECTFOLDER}"
 
 # install Composer
 curl -s https://getcomposer.org/installer | php
